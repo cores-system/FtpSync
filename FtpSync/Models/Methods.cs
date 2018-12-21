369593ece7ca07ad99dc31bd40e60f220660ece0
cores-system <dev@sped-mail.ru>
2018-12-21 13:12:54 +0200

    Updated comment

--- FtpSync/Models/Methods.cs
+++ FtpSync/Models/Methods.cs
@@ -2,9 +2,9 @@
 {
     public enum Methods
     {
-        errorMsg,
-        uploadFile,
-        syncGood,
-        lastSyncGood
+        errorMsg,      // Синхронизация не запущена из за ошибки 
+        uploadFile,    // Данные загруженого файла, статус, папки и т.д
+        syncGood,      // Если false то один из файлов не загрузился
+        lastSyncGood   // Время успешной отметки
     }
 }
